09b225f28754eccc5a43d4a14d395bcfb771a3c5
Jaxon7Cheung <2104526824@qq.com>
2024-06-15 03:17:10 +0800

    OC对象源码

---
+++ OC对象的本质/OC对象的本质/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OC对象的本质
+//
+//  Created by 张旭洋 on 2024/6/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
