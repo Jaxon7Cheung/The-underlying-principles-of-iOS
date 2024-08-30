65344c00faf566b0f3c7ce319ca2722ed62a6e25
Jaxon7Cheung <2104526824@qq.com>
2024-08-30 15:31:55 +0800

    消息转发机制兜底（统一拦截方法调用）

--- Runtime/super关键字/super关键字/Person.m
+++ Runtime/super关键字/super关键字/Person.m
@@ -9,8 +9,23 @@
 
 @implementation Person
 
-- (void)run {
-    NSLog(@"%s", __func__);
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    
+    // 本来能调用的方法
+    if ([self respondsToSelector: aSelector]) {
+        return [super methodSignatureForSelector: aSelector];
+    }
+    
+    // 找不到方法
+    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
 }
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    NSLog(@"找不到%@方法", NSStringFromSelector(anInvocation.selector));
+}
+
+//- (void)run {
+//    NSLog(@"%s", __func__);
+//}
 
 @end
--- Runtime/super关键字/super关键字/main.m
+++ Runtime/super关键字/super关键字/main.m
@@ -8,9 +8,27 @@
 #import <Foundation/Foundation.h>
 #import "Student.h"
 
+//void test(SEL selector) {
+//    Person* person = [[Person alloc] init];
+//    
+//    [person performSelector: selector];
+//}
+
+//void test(NSMutableArray* array) {
+//    [array addObject: @"234"];
+//}
+
+// 降低unrecognized selector崩溃率
+// 使用消息转发机制实现一个方法兜底，可以给NSObject写一个分类，在分类里加上兜底操作
+// 统一拦截某一个方法的调用
+// 后面学习NSProxy虚基类处理消息转发
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Student* student = [[Student alloc] init];
+//        Student* student = [[Student alloc] init];
+        
+        Person* person = [Person new];
+        [person run];
         
     }
     return 0;
